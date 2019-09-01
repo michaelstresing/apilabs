0770715e25459f10b0b7dd001d445741a256e7f1
michaelstresing <mstresing@gmail.com>
2019-09-01 16:33:35 +0100

    starting on lab4

--- databases/Exercise_04.py
+++ databases/Exercise_04.py
@@ -16,3 +16,42 @@ The more dynamic the application, the better!
 
 
 '''
+'''
+TODO: 
+heaps of stuff..
+'''
+
+import sqlalchemy
+from sqlalchemy import Column, MetaData, Table, Integer, String
+
+passw = ""
+engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{passw}@localhost/freshdb4lab')
+connection = engine.connect()
+meta = MetaData()
+
+
+def create_table():
+
+
+    tablename = str(input('Please enter a name for the table: '))
+    usertable = Table(
+        f'{tablename}', meta,
+        Column('', Integer, primary_key=True),
+        Column('', String),
+        Column('', String)
+    )
+
+    cols = int(input('Please enter the number of columns for your table: '))
+
+    for col in range(cols - 1):
+        colname = str(input('Please enter the name of the first column: '))
+        coltype = str(input(f'Please enter the data type for {colname} (s)tring, (i)nt, (d)atetime.'))
+
+        if coltype == "s":
+
+        usertable.
+
+meta.create_all(engine)
+
+def insert_data():
+
